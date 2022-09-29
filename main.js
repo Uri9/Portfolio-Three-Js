3d4d82e939d9a91c962de087f1736f4e6a024222
Uri9 <019001186@upsrj.edu.mx>
2022-09-29 14:34:06 -0500

    Second commit

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ Array(200).fill().forEach(addStar)
 const spaceTexture = new THREE.TextureLoader().load('space.jpg')
 scene.background = spaceTexture
 
-const jeffTexture = new THREE.TextureLoader().load('andy.jpg')
+const jeffTexture = new THREE.TextureLoader().load('jeff.jpg')
 
 const jeff = new THREE.Mesh(
   new THREE.BoxGeometry(3, 3, 3),
